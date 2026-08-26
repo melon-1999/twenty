848e8a844615bf3ff28727a5f891050f7a2d2ff0
Janis Melon <janismelon@web.de>
2026-08-26 20:20:20 +0200

    feat(front): forecast nav link + de-DE strings

--- packages/twenty-front/src/modules/navigation/components/MainNavigationDrawerScrollableItems.tsx
+++ packages/twenty-front/src/modules/navigation/components/MainNavigationDrawerScrollableItems.tsx
@@ -1,9 +1,16 @@
 import { NavigationDrawerOpenedSection } from '@/navigation-menu-item/display/sections/components/NavigationDrawerOpenedSection';
 import { NavigationDrawerWorkspaceSectionSkeletonLoader } from '@/object-metadata/components/NavigationDrawerWorkspaceSectionSkeletonLoader';
+import { objectMetadataItemsSelector } from '@/object-metadata/states/objectMetadataItemsSelector';
+import { NavigationDrawerItem } from '@/ui/navigation/navigation-drawer/components/NavigationDrawerItem';
+import { useAtomStateValue } from '@/ui/utilities/state/jotai/hooks/useAtomStateValue';
 
 import { styled } from '@linaria/react';
+import { t } from '@lingui/core/macro';
 import { lazy, Suspense } from 'react';
+import { useLocation } from 'react-router-dom';
+import { AppPath, CoreObjectNameSingular } from 'twenty-shared/types';
 
+import { IconChartBar } from 'twenty-ui/icon';
 import { themeCssVariables } from 'twenty-ui/theme-constants';
 
 const FavoritesSectionDispatcher = lazy(() =>
@@ -29,6 +36,14 @@ const StyledScrollableItemsContainer = styled.div`
 `;
 
 export const MainNavigationDrawerScrollableItems = () => {
+  const objectMetadataItems = useAtomStateValue(objectMetadataItemsSelector);
+  const { pathname } = useLocation();
+
+  const hasOpportunityObject = objectMetadataItems.some(
+    (objectMetadataItem) =>
+      objectMetadataItem.nameSingular === CoreObjectNameSingular.Opportunity,
+  );
+
   return (
     <StyledScrollableItemsContainer>
       <NavigationDrawerOpenedSection />
@@ -36,6 +51,14 @@ export const MainNavigationDrawerScrollableItems = () => {
         <FavoritesSectionDispatcher />
         <WorkspaceSectionDispatcher />
       </Suspense>
+      {hasOpportunityObject && (
+        <NavigationDrawerItem
+          label={t`Forecast`}
+          to={AppPath.ForecastPage}
+          Icon={IconChartBar}
+          active={pathname === AppPath.ForecastPage}
+        />
+      )}
     </StyledScrollableItemsContainer>
   );
 };
